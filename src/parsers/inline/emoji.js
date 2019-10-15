80b1e3700bf34ebffd1fc0c23f5a142b11289e41
Nipinium <nipinium@gmail.com>
2019-10-15 19:44:02 +0700

    parse emoji

---
+++ src/parsers/inline/emoji.js
@@ -0,0 +1,16 @@
+module.exports = function scan_emoji(input, offset) {
+    const output = { tag: 'emoji', body: [] }
+
+    offset += 1
+    for (; offset < input.length; offset++) {
+        let curr = input[offset]
+        if (curr[0] === ':') break
+        else if (/[^\w]/.test(curr[0])) return null
+        else output.body.push(curr)
+    }
+
+    // console.log({ output })
+    if (offset < input.length && output.body.length > 0)
+        return { output, offset }
+    return null
+}
---
+++ src/parsers/inline/emoji.test.js
@@ -0,0 +1,33 @@
+const parse_emoji = require('./emoji')
+
+const { tokenize } = require('../utils')
+
+function parse(input) {
+    const tokens = tokenize(input)
+    return parse_emoji(tokens, 0)
+}
+
+function body_for(str) {
+    return Array.from(str).map(x => [x, x])
+}
+
+test('valid emoji', () => {
+    const inp = ':smile:'
+    const out = {
+        output: {
+            tag: 'emoji',
+            body: body_for('smile'),
+        },
+        offset: inp.length - 1,
+    }
+
+    expect(parse(inp)).toEqual(out)
+})
+
+test('invalid emoji', () => {
+    expect(parse('::')).toEqual(null)
+    expect(parse(': :')).toEqual(null)
+    expect(parse(':x :')).toEqual(null)
+    expect(parse(': x:')).toEqual(null)
+    expect(parse(':x x:')).toEqual(null)
+})
