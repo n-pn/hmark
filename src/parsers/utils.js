e1381cff27a98cd75d68f7fcae7085c353ab8ee0
Nipinium <nipinium@gmail.com>
2019-10-15 11:48:59 +0700

    done: parse utils

---
+++ src/parsers/utils.js
@@ -0,0 +1,99 @@
+exports.parse_attrs = input => {
+    if (typeof input === 'string') input = exports.tokenize(input)
+
+    let output = {}
+
+    for (let i = 0; i < input.length; i++) {
+        while (i < input.length && input[i][0] === ' ') i += 1
+        if (i >= input.length) break
+
+        let key = ''
+
+        for (; i < input.length; i++) {
+            let mark = input[i][0]
+            if (mark === '=' || mark === ' ') break
+            key += input[i][1]
+        }
+
+        if (i + 1 >= input.length) {
+            output[key] = true
+            break
+        } else if (input[i][0] === ' ') {
+            output[key] = true
+            continue
+        } else {
+            i += 1 // skip '='
+        }
+
+        let stop_char = input[i][0]
+        if (stop_char === '"' || stop_char === "'") i += 1
+        else stop_char = ' '
+
+        let value = ''
+        for (; i < input.length && input[i][0] !== stop_char; i++) {
+            value += input[i][1]
+        }
+
+        output[key] = value === '' ? true : value
+    }
+
+    return output
+}
+
+exports.tokenize = input => {
+    const chars = Array.from(input)
+
+    let output = []
+
+    for (i = 0; i < chars.length; i++) {
+        let char = chars[i]
+        let next = chars[i + 1]
+
+        if (char === '\\' && may_escape(next)) {
+            output.push([char, next])
+            i += 1 // skip reading next
+        } else {
+            output.push([char, char])
+        }
+    }
+
+    return output
+}
+
+function may_escape(char) {
+    switch (char) {
+        // case '~':
+        // case '$':
+        // case '%':
+        // case '&':
+        // case ',':
+        // case '.':
+        // case ';':
+        // case '?':
+        // case '|':
+        case '"': // double quote
+        case "'": // single quote
+        case '#': // hash tag
+        case '@': // at tag
+        case '[': // task list, link text, custom tag
+        case ']': // close task list or link text, close custom tag
+        case '!': // mark link is image
+        case '(': // open link text
+        case ')': // close link text
+        case '<': // before text link
+        case '>': // block quote, close text link
+        case '=': // heading
+        case '+': // auto ordered list
+        case '-': // unordered list, rulers
+        case '`': // code
+        case '*': // bold
+        case '_': // italic
+        case ':': // emoji
+        case ' ': // escape whitespace
+        case '/': // escape tag terminal
+        case '\\': // escape self
+            return true
+        default:
+            return false
+    }
+}
---
+++ src/parsers/utils.test.js
@@ -0,0 +1,58 @@
+const utils = require('./utils')
+
+describe('parse attributes', () => {
+    test('return empty', () => {
+        expect(utils.parse_attrs('     ')).toEqual({})
+    })
+
+    test('only attribute keys', () => {
+        const inp = '   a  b         c d         '
+        const out = { a: true, b: true, c: true, d: true }
+        expect(utils.parse_attrs(inp)).toEqual(out)
+    })
+
+    test('mixed delimiters', () => {
+        const inp = '   a=a  b="b"         c d="d\\"d"'
+        const out = { a: 'a', b: 'b', c: true, d: 'd"d' }
+        expect(utils.parse_attrs(inp)).toEqual(out)
+    })
+
+    test('for tokenized input', () => {
+        const inp = utils.tokenize('   a=a  b="b"         c d="d\\"d"')
+        const out = { a: 'a', b: 'b', c: true, d: 'd"d' }
+        expect(utils.parse_attrs(inp)).toEqual(out)
+    })
+})
+
+describe('tokenizer', () => {
+    test('keeps normal character', () => {
+        const inp = 'abcde'
+        const out = Array.from(inp).map(x => [x, x])
+
+        expect(utils.tokenize(inp)).toEqual(out)
+    })
+
+    test('characters that should not be escaped', () => {
+        const inp = '\\a\\b\\c\\1\\2\\3\\~'
+        const out = Array.from(inp).map(x => [x, x])
+
+        expect(utils.tokenize(inp)).toEqual(out)
+    })
+
+    test('escapes special characters', () => {
+        const inp = '\\+\\-\\*\\_\\[\\]\\/\\\\'
+
+        const out = [
+            ['\\', '+'],
+            ['\\', '-'],
+            ['\\', '*'],
+            ['\\', '_'],
+            ['\\', '['],
+            ['\\', ']'],
+            ['\\', '/'],
+            ['\\', '\\'],
+        ]
+
+        expect(utils.tokenize(inp)).toEqual(out)
+    })
+})
