59c1a2dc5bc026c27875060a68789d3e678c8bb8
Nipinium <nipinium@gmail.com>
2020-01-09 20:08:46 +0700

    switch from babel to typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,30 @@
+import typescript from '@rollup/plugin-typescript'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import babel from 'rollup-plugin-babel'
 import { uglify } from 'rollup-plugin-uglify'
 import pkg from './package.json'
 
+const ts = typescript({ lib: ['es5', 'es6', 'dom'], target: 'es5' })
 export default [
     // browser-friendly UMD build
     {
-        input: 'src/main.js',
+        input: 'src/main.ts',
         output: {
-            name: 'rollupJestBoilerplate',
+            name: 'hmark',
             file: pkg.browser,
             format: 'umd',
         },
-        plugins: [
-            resolve(),
-            commonjs(),
-            babel({
-                exclude: 'node_modules/**',
-            }),
-            uglify(),
-        ],
+        plugins: [ts, resolve(), commonjs(), uglify()],
     },
 
     // CommonJS (for Node) and ES module (for bundlers) build.
     {
-        input: 'src/main.js',
+        input: 'src/main.ts',
         external: [],
         output: [
             { file: pkg.main, format: 'cjs' },
             { file: pkg.module, format: 'es' },
         ],
+        plugins: [ts],
     },
 ]
